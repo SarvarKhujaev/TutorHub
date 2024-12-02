d8a8dbc369d7c8e2b807dc0dc76c05febceb372c
SKhuzhaev <skhuzhaev@beeline.uz>
2024-12-02 11:58:47 +0500

    first commit

---
+++ src/main/java/org/tutorhub/TutorHubApplication.java
@@ -0,0 +1,11 @@
+package org.tutorhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorHubApplication {
+	public static void main( final String[] args ) {
+		SpringApplication.run( TutorHubApplication.class, args );
+	}
+}
---
+++ src/test/java/org/tutorhub/TutorHubApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tutorhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
